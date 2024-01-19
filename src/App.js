12ff2fcc0b40a08fca784a80b2bef9c365ebae79
mekaala <mekaala@gmail.com>
2024-01-19 10:45:49 -0500

    added directional movement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Suspense } from "react";
-import { Canvas, useLoader } from "react-three-fiber";
+import React, { Suspense, useRef, useState } from "react";
+import { Canvas, useFrame, useLoader } from "react-three-fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import "./App.css"
+import { TextureLoader } from "three";
 
 function Loading() {
   return (
@@ -20,9 +21,28 @@ function Loading() {
 }
 
 function ArWing() {
+  const [shipPosition, setShipPosition] = useState();
+
+  const ship = useRef();
+  useFrame(({ mouse }) => {
+    setShipPosition({
+      position: { x: mouse.x * 6, y: mouse.y * 2 },
+      rotation: { z: -mouse.x * 0.5, x: -mouse.x * 0.5, y: -mouse.y * 0.2 },
+    });
+  });
+  // Update the ships position from the updated state.
+  useFrame(() => {
+    ship.current.rotation.z = shipPosition.rotation.z;
+    ship.current.rotation.y = shipPosition.rotation.x;
+    ship.current.rotation.x = shipPosition.rotation.y;
+    ship.current.position.y = shipPosition.position.y;
+    ship.current.position.x = shipPosition.position.x;
+  });
+  
   const { nodes } = useLoader(GLTFLoader, "models/arwing.glb");
+
   return (
-    <group>
+    <group ref={ship}>
       <mesh visible geometry={nodes.Default.geometry}>
         <meshStandardMaterial
           attach="material"
@@ -35,6 +55,60 @@ function ArWing() {
   );
 }
 
+const groundHeight = -50;
+
+function Terrain() {
+  const terrain = useRef();
+  useFrame(() => {
+    terrain.current.position.z += 0.4
+  });
+  return (
+    <mesh
+      visible
+      position={[0, groundHeight, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      ref={terrain}
+    >
+      <planeGeometry attach="geometry" args={[5000, 5000, 128, 128]}/>
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        roughness={1}
+        metalness={0}
+        wireframe
+      />
+    </mesh>
+  );
+}
+
+function Target() {
+  const rearTarget = useRef();
+  const frontTarget = useRef();
+
+  const loader = new TextureLoader();
+
+  const texture = loader.load("target.png")
+
+  useFrame(({ mouse }) => {
+    rearTarget.current.position.y = -mouse.y * 10;
+    rearTarget.current.position.x = -mouse.x * 30;
+
+    frontTarget.current.position.y = -mouse.y * 20;
+    frontTarget.current.position.x = -mouse.x * 60;
+  });
+
+  return (
+    <group>
+      <sprite position={[0, 0, -8]} ref={rearTarget}>
+        <spriteMaterial attach="material" map={texture} />
+      </sprite>
+      <sprite position={[0, 0, -16]} ref={frontTarget}>
+        <spriteMaterial attach="material" map={texture} />
+      </sprite>
+    </group>
+  );
+}
+
 function App() {
   return (
     <Canvas style={{ background: "#171717" }}>
@@ -42,6 +116,8 @@ function App() {
       <Suspense fallback={<Loading />}>
         <ArWing />
       </Suspense>
+      <Target />
+      <Terrain/>
     </Canvas>
   );
 }
