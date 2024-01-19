3cff5afdb63681f0bec483b52b27222306242efd
mekaala <mekaala@gmail.com>
2024-01-19 09:53:51 -0500

    implemented code to display arwing

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Suspense } from "react";
+import { Canvas, useLoader } from "react-three-fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import "./App.css"
+
+function Loading() {
+  return (
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        transparent
+        opacity={0.6}
+        roughness={1}
+        metalness={0}
+      />
+    </mesh>
+  );
+}
+
+function ArWing() {
+  const { nodes } = useLoader(GLTFLoader, "models/arwing.glb");
+  return (
+    <group>
+      <mesh visible geometry={nodes.Default.geometry}>
+        <meshStandardMaterial
+          attach="material"
+          color="white"
+          roughness={0.3}
+          metalness={0.3}
+        />
+      </mesh>
+    </group>
+  );
+}
+
+function App() {
+  return (
+    <Canvas style={{ background: "#171717" }}>
+      <directionalLight intensity={0.5} />
+      <Suspense fallback={<Loading />}>
+        <ArWing />
+      </Suspense>
+    </Canvas>
+  );
+}
+
+export default App;
